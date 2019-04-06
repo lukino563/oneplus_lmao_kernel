4ac32d19f91e9004c6af4cf0b019714980c7a046
Erik Müller <erik.lars.mueller@googlemail.com>
2019-04-06 19:32:28 +0200

    unified: gpu: drm: force fp pressed if fp mode on

--- drivers/gpu/drm/msm/sde/sde_crtc.c
+++ drivers/gpu/drm/msm/sde/sde_crtc.c
@@ -66,6 +66,7 @@
 #include <linux/sysfs.h>
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/set_os.h>
 #include <drm/drm_mipi_dsi.h>
 extern int msm_drm_notifier_call_chain(unsigned long val, void *v);
 
@@ -5607,17 +5608,19 @@ static int sde_crtc_onscreenfinger_atomic_check(struct sde_crtc_state *cstate,
 	else
 		display->panel->dim_status = false;
 
-	if (fp_mode == 1 && sde_crtc_config_fingerprint_dim_layer(&cstate->base, 5)) {
-		pr_err("Failed to config dim layer\n");
-		return -EINVAL;
-	}
-	if (fp_mode == 1) {
-		cstate->fingerprint_pressed = true;
-		return 0;
-	} else {
-		cstate->fingerprint_pressed = false;
-		cstate->fingerprint_dim_layer = NULL;
-		return 0;
+	if (!is_oos()) {
+		if (fp_mode == 1 && sde_crtc_config_fingerprint_dim_layer(&cstate->base, 5)) {
+			pr_err("Failed to config dim layer\n");
+			return -EINVAL;
+		}
+		if (fp_mode == 1) {
+			cstate->fingerprint_pressed = true;
+			return 0;
+		} else {
+			cstate->fingerprint_pressed = false;
+			cstate->fingerprint_dim_layer = NULL;
+			return 0;
+		}
 	}
 
 	if(aod_index <0)
