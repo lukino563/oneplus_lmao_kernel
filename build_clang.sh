12770ac1cfbc473ea2850c5a93f9122e1e38d847
Erik Müller <erik.lars.mueller@googlemail.com>
2019-06-03 19:30:24 +0200

    Version 1.0.0

--- build_clang.sh
+++ build_clang.sh
@@ -12,16 +12,16 @@ clear
 THREAD="-j$(grep -c ^processor /proc/cpuinfo)"
 KERNEL="Image"
 DTBIMAGE="dtb"
-export CLANG_PATH=~/android/Toolchains/clang/clang-r328903/bin/
+export CLANG_PATH=~/android/Toolchains/LLVM806/bin/
 export PATH=${CLANG_PATH}:${PATH}
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=${HOME}/android/Toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android- CC=clang CXX=clang++
 export CROSS_COMPILE_ARM32=${HOME}/android/Toolchains/arm-linux-androideabi-4.9/bin/arm-linux-androideabi- 
-export KBUILD_COMPILER_STRING=$(~/android/Toolchains/clang/clang9.0.2/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+export KBUILD_COMPILER_STRING=$(~/android/Toolchains/LLVM806/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
 DEFCONFIG="smurf_defconfig"
 
 # Kernel Details
-VER=".1.0.0.clang"
+VER=".1.0.0"
 
 # Paths
 KERNEL_DIR=`pwd`
