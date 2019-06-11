9cc56a43867fa21ea8cbbcd7d65ba6abe02a70fc
Erik Müller <erik.lars.mueller@googlemail.com>
2019-06-11 21:07:28 +0200

    add gcc9 build tools

--- build_gcc9.sh
+++ build_gcc9.sh
@@ -12,16 +12,17 @@ clear
 THREAD="-j$(grep -c ^processor /proc/cpuinfo)"
 KERNEL="Image"
 DTBIMAGE="dtb"
+
 #export CLANG_PATH=~/android/Toolchains/clang/clang-r328903/bin/
 #export PATH=${CLANG_PATH}:${PATH}
 #export CLANG_TRIPLE=aarch64-linux-gnu-
-export CROSS_COMPILE=${HOME}/android/Toolchains/gcc9arm64/bin/aarch64-elf-
+export CROSS_COMPILE=${HOME}/android/Toolchains/gcc9arm64/bin/aarch64-linux-gnu-
 export CROSS_COMPILE_ARM32=${HOME}/android/Toolchains/gcc9eabi/bin/arm-eabi-
 #export KBUILD_COMPILER_STRING=$(~/android/Toolchains/clang/clang-r328903/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
 DEFCONFIG="smurf_defconfig"
 
 # Kernel Details
-VER=".1.0.1t3"
+VER=".1.2.0t2"
 
 # Paths
 KERNEL_DIR=`pwd`
@@ -44,7 +45,7 @@ function clean_all {
 }
 
 function make_kernel {
-	      #cp ~/android/SmurfKernelOP7/Makefile.clang ~/android/op7/Makefile
+	      cp ~/android/SmurfKernelOP7/Makefile.gcc9 ~/android/SmurfKernelOP7/Makefile
 	      echo
               make ARCH=arm64 O=out $DEFCONFIG
               make ARCH=arm64 O=out $THREAD
