9ae2c2018182e268c5c6d8ef43b469af6261ea8e
Erik Müller <erik.lars.mueller@googlemail.com>
2019-06-22 17:55:33 +0200

    cpu_input_boost: remove boost block

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -135,8 +135,6 @@ struct boost_drv {
 	unsigned int max_boost_ms;
 	unsigned long state;
 	unsigned long stune_state;
-	atomic_long_t cluster1_boost_expires;
-	atomic_long_t cluster2_boost_expires;
 };
 
 static void input_unboost_worker(struct work_struct *work);
@@ -359,16 +357,6 @@ void cpu_input_boost_kick_cluster1(unsigned int duration_ms)
 	if (!test_bit(SCREEN_ON, &b->state))
 		return;
 
-	do {
-		curr_expires = atomic_long_read(&b->cluster1_boost_expires);
-		new_expires = jiffies + boost_jiffies;
-
-		/* Skip this boost if there's a longer boost in effect */
-		if (time_after(curr_expires, new_expires))
-			return;
-	} while (atomic_long_cmpxchg(&b->cluster1_boost_expires, curr_expires,
-				     new_expires) != curr_expires);
-	
 	__cpu_input_boost_kick_cluster1(b, duration_ms);
 }
 
@@ -383,16 +371,6 @@ void cpu_input_boost_kick_cluster2(unsigned int duration_ms)
 
 	if (!test_bit(SCREEN_ON, &b->state))
 		return;
-
-	do {
-		curr_expires = atomic_long_read(&b->cluster2_boost_expires);
-		new_expires = jiffies + boost_jiffies;
-
-		/* Skip this boost if there's a longer boost in effect */
-		if (time_after(curr_expires, new_expires))
-			return;
-	} while (atomic_long_cmpxchg(&b->cluster2_boost_expires, curr_expires,
-				     new_expires) != curr_expires);
 
 	__cpu_input_boost_kick_cluster2(b, duration_ms);
 }
@@ -434,16 +412,6 @@ void cpu_input_boost_kick_cluster1_wake(unsigned int duration_ms)
 	if (test_bit(SCREEN_ON, &b->state))
 		return;
 
-	do {
-		curr_expires = atomic_long_read(&b->cluster1_boost_expires);
-		new_expires = jiffies + boost_jiffies;
-
-		/* Skip this boost if there's a longer boost in effect */
-		if (time_after(curr_expires, new_expires))
-			return;
-	} while (atomic_long_cmpxchg(&b->cluster1_boost_expires, curr_expires,
-				     new_expires) != curr_expires);
-
 	__cpu_input_boost_kick_cluster1_wake(b, duration_ms);
 }
 
@@ -458,16 +426,6 @@ void cpu_input_boost_kick_cluster2_wake(unsigned int duration_ms)
 
 	if (test_bit(SCREEN_ON, &b->state))
 		return;
-
-	do {
-		curr_expires = atomic_long_read(&b->cluster2_boost_expires);
-		new_expires = jiffies + boost_jiffies;
-
-		/* Skip this boost if there's a longer boost in effect */
-		if (time_after(curr_expires, new_expires))
-			return;
-	} while (atomic_long_cmpxchg(&b->cluster2_boost_expires, curr_expires,
-				     new_expires) != curr_expires);
 
 	__cpu_input_boost_kick_cluster2_wake(b, duration_ms);
 }
@@ -575,21 +533,15 @@ static int cpu_boost_thread(void *data)
 
 	sched_setscheduler_nocheck(current, SCHED_FIFO, &sched_max_rt_prio);
 
-	while (1) {
-		bool should_stop = false;
+	while (!kthread_should_stop()) {
 		unsigned long curr_state;
 
 		wait_event(b->boost_waitq,
 			(curr_state = READ_ONCE(b->state)) != old_state ||
-			(should_stop = kthread_should_stop()));
-
-		if (should_stop)
-			break;
-
+			kthread_should_stop());
 		old_state = curr_state;
 		update_online_cpu_policy();
 	}
-
 	return 0;
 }
 
