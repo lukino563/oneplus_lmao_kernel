91918962fcbdd7dfa73c667c034dd836361d7142
Erik Müller <erik.lars.mueller@googlemail.com>
2019-06-30 12:09:06 +0200

    Version 1.9.0

--- build_gcc9_wifi_drv.sh
+++ build_gcc9_wifi_drv.sh
@@ -23,7 +23,7 @@ export CROSS_COMPILE_ARM32=${HOME}/android/Toolchains/gcc9eabi/bin/arm-eabi-
 DEFCONFIG="smurfwdrv_defconfig"
 
 # Kernel Details
-VER=".1.8.1wdrv"
+VER=".1.9.0wdrv"
 
 # Paths
 KERNEL_DIR=`pwd`
