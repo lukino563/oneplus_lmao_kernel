f5d4ac960a092a81d05d36d655a6f770a0a13ed8
Erik Müller <erik.lars.mueller@googlemail.com>
2019-07-14 17:25:23 +0200

    GPU boost 200 -> 180 MHz

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -41,7 +41,7 @@ static unsigned int remove_input_boost_freq_perf __read_mostly = CONFIG_REMOVE_I
 static unsigned int remove_input_boost_freq_gold __read_mostly = CONFIG_REMOVE_INPUT_BOOST_FREQ_GOLD;
 static unsigned int gpu_boost_freq __read_mostly = CONFIG_GPU_BOOST_FREQ;
 static unsigned int gpu_min_freq __read_mostly = CONFIG_GPU_MIN_FREQ;
-static unsigned int gpu_sleep_freq __read_mostly = 200; 
+static unsigned int gpu_sleep_freq __read_mostly = 180; 
 static unsigned short input_boost_duration __read_mostly = CONFIG_INPUT_BOOST_DURATION_MS;
 static unsigned short flex_boost_duration __read_mostly = CONFIG_FLEX_BOOST_DURATION_MS;
 static  unsigned int input_thread_prio __read_mostly = CONFIG_INPUT_THREAD_PRIORITY;
@@ -271,7 +271,7 @@ static void update_gpu_boost(struct boost_drv *b, int freq)
 		level=6;
 	if (freq==257)
 		level=7;
-	if (freq==200)
+	if (freq==180)
 		level=8;
 	b->gpu_pwr->min_pwrlevel=level;
 }
@@ -634,6 +634,7 @@ static int msm_drm_notifier_cb(struct notifier_block *nb,
 		cpu_input_boost_kick_cluster1_wake(1000);
 		cpu_input_boost_kick_cluster2_wake(1000);	
 		set_bit(SCREEN_ON, &b->state);
+		update_gpu_boost(b, gpu_min_freq);
 	} else if (*blank == MSM_DRM_BLANK_POWERDOWN_CUST) {
 		clear_bit(SCREEN_ON, &b->state);
 		clear_bit(INPUT_BOOST, &b->state);
@@ -645,6 +646,7 @@ static int msm_drm_notifier_cb(struct notifier_block *nb,
 		clear_bit(INPUT_STUNE_BOOST, &b->state);
 		clear_bit(MAX_STUNE_BOOST, &b->state);
 		clear_bit(FLEX_STUNE_BOOST, &b->state);
+		update_gpu_boost(b, gpu_sleep_freq);
 		pr_info("Screen off, boosts turned off\n");
 		wake_up(&b->boost_waitq);
 	}
@@ -678,6 +680,7 @@ static int fb_notifier_cb(struct notifier_block *nb, unsigned long action,
 		clear_bit(INPUT_STUNE_BOOST, &b->state);
 		clear_bit(MAX_STUNE_BOOST, &b->state);
 		clear_bit(FLEX_STUNE_BOOST, &b->state);
+		pr_info("Screen off, boosts turned off\n");
 		update_gpu_boost(b, gpu_sleep_freq);
 		wake_up(&b->boost_waitq);
 	}
